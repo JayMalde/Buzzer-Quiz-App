6e1b757a731b7fe22ae5c0e9cea0ea7a067a41af
Jay Malde <maldejay77@gmail.com>
2021-11-16 11:52:10 +0530

    updated code

--- index.js
+++ index.js
@@ -54,4 +54,4 @@ io.on('connection', (socket) => {
  
 })
 
-server.listen(3000, () => console.log('Listening on 3000'));
+server.listen((process.env.PORT || 5000), () => console.log('Listening on 3000'));
